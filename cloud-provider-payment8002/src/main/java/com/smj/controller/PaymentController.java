a60c0b66ea692345e79ccfab968f4352984e1ad9
sumaojin <sumaojin@meb.com>
2020-08-25 18:02:31 +0800

    增加了eureka的集群和payment的集群

--- cloud-provider-payment8002/src/main/java/com/smj/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/smj/controller/PaymentController.java
@@ -5,10 +5,13 @@ import com.smj.entities.Payment;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import com.smj.service.PaymentService;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @Author: Administrator
@@ -26,12 +29,15 @@ public class PaymentController {
     @Value("${server.port}")
     private String serverPort;
 
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/get/{id}")
     public CommonResult getPayment(@PathVariable Long id){
         Payment payment = paymentService.getPaymentById(id);
-        log.info("获取的payment:", payment);
+        log.info("获取的payment:" + payment);
         if (payment != null){
-            return new CommonResult(200, "插入成功-serverPort:"+serverPort, payment);
+            return new CommonResult(200, "获取成功-serverPort:"+serverPort, payment);
         }
         return new CommonResult(444, "没有对应记录");
     }
@@ -39,11 +45,22 @@ public class PaymentController {
     @PostMapping("/create")
     public CommonResult createPayment(@RequestBody Payment payment){
         Integer i = paymentService.create(payment);
-        log.info("保存成功", i);
+        log.info("保存成功"+ i);
         if (i > 0){
-            return new CommonResult(200, "创建成功", i);
+            return new CommonResult(200, "创建成功-serverPort:"+serverPort, i);
         }else{
             return new CommonResult(444, "创建失败");
         }
+    }
+
+    @GetMapping("/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        services.forEach(s -> log.info("******element:"+s));
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        instances.forEach(i -> log.info(i.getServiceId() + "\t" + i.getHost() + "\t" + i.getPort() + "\t" + i.getUri()));
+
+        return this.discoveryClient;
     }
 }
