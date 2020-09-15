4110b398f04310f8abf33c7b17d4ad3a5433c8c6
sumaojin <sumaojin@meb.com>
2020-09-15 17:16:17 +0800

    增加了eureka的集群和payment的集群

--- cloud-consumer-order80/src/main/java/com/smj/OrderApplication80.java
+++ cloud-consumer-order80/src/main/java/com/smj/OrderApplication80.java
@@ -1,9 +1,11 @@
 package com.smj;
 
+import myrule.MyselfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @Author: Administrator
@@ -12,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+//@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MyselfRule.class)
 public class OrderApplication80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication80.class, args);
