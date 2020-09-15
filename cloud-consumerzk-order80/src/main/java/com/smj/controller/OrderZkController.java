3f0618f0a7bb661488b7206890911f381407ae31
sumaojin <sumaojin@meb.com>
2020-09-15 14:48:18 +0800

    增加了eureka的集群和payment的集群

---
+++ cloud-consumerzk-order80/src/main/java/com/smj/controller/OrderZkController.java
@@ -0,0 +1,26 @@
+package com.smj.controller;
+
+import com.smj.entities.CommonResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+@RequestMapping("/consumer")
+public class OrderZkController {
+    private static final String INVOKE_URL="http://cloud-payment-service";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/payment/zk")
+    public String paymentInfo(){
+        String result = restTemplate.getForObject(INVOKE_URL+"/payment/zk",String.class);
+        return result;
+    }
+}
